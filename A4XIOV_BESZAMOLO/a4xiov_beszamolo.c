893af8d64b446a21fceb7fbbea008d9e8dc1bdfc
APeterIstvan <79099771+APeterIstvan@users.noreply.github.com>
2021-05-10 11:56:44 +0200

    Add files via upload

--- A4XIOV_BESZAMOLO/a4xiov_beszamolo.c
+++ A4XIOV_BESZAMOLO/a4xiov_beszamolo.c
@@ -6,20 +6,22 @@
 
 int main(){
         int child;
-
         child = fork();
+
+        char *path = "execl_prog";
+        setenv("PATH", path, 1);
 
         if(child < 0)
                 perror("fork error");
         if(child == 0){
-                if(execl("./execl_prog.out", "execl_prog", (char*) NULL)<0)
+                if(execl("./execl_prog.out", getenv("PATH"), (char*) NULL)<0)
                                 perror("execl error");
         }
 
         if(waitpid(child, NULL, 0)<0)
                 perror("wait error");
 
-        printf("Szülő vagyok.\nPID-em: %d\nGyermekem PID-je: %d\n", getpid(), getppid());
+        printf("Szülő vagyok.\nPID-em: %d\nGyermekem PID-je: %d\n", getpid(), child);
 
 
         return 0;
